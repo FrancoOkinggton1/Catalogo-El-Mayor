64af98419f755610c8f67fa0126da044ee9668fb
Franco <franco.valdebenito.o@mail.pucv.cl>
2026-06-07 22:31:16 -0400

    ola x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  // Cambia "tu-usuario" por tu nombre de usuario en GitHub
+  site: 'https://FrancoOkinggton1.github.io',
+  // Cambia "elmayor" por el nombre exacto de tu repositorio
+  base: 'elmayor',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
